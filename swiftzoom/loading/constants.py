e23bd1b53e1edbc5177f7a4888e99ba79e33a7ea
Edoardo Altamura <38359901+edoaltamura@users.noreply.github.com>
2023-08-23 17:51:05 +0200

    Create constants.py

---
+++ swiftzoom/loading/constants.py
@@ -0,0 +1,11 @@
+# Constants
+mean_molecular_weight = 0.5954                  # Mean atomic weight for an ionized gas with primordial composition (X = 0.76, Z = 0)
+mean_atomic_weight_per_free_electron = 1.14
+primordial_hydrogen_mass_fraction = 0.76
+solar_metallicity = 0.0133714
+
+# Aliases
+mu_bar = mean_molecular_weight
+mu_e_bar = mean_atomic_weight_per_free_electron
+x_h_primordial = primordial_hydrogen_mass_fraction
+z_sun = solar_metallicity
