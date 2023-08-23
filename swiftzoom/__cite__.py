c08babfb552f8b3da86b66c4e4c2006cf1ab1159
Edoardo Altamura <38359901+edoaltamura@users.noreply.github.com>
2023-08-23 18:01:48 +0200

    Update __cite__.py

--- swiftzoom/__cite__.py
+++ swiftzoom/__cite__.py
@@ -1,7 +1,27 @@
-__cite__ = """@article{Altamura2023,
-  year = {2023},
-  publisher = {},
-  author = {Edoardo Altamura},
-  title = {swiftzoom},
-}
-"""
+__cite__ = [
+  """@ARTICLE{Altamura2023,
+    year = {2023},
+    journal = {The Journal of Open Source Software},
+    author = {{Altamura}, Edoardo},
+    title = "{swiftzoom: A Python library for analyzing zoom-in SWIFT simulation data}",
+  }
+  """,
+  """@ARTICLE{2023MNRAS.520.3164A,
+         author = {{Altamura}, Edoardo and {Kay}, Scott T. and {Bower}, Richard G. and {Schaller}, Matthieu and {Bah{\'e}}, Yannick M. and {Schaye}, Joop and {Borrow}, Josh and {Towler}, Imogen},
+          title = "{EAGLE-like simulation models do not solve the entropy core problem in groups and clusters of galaxies}",
+        journal = {\mnras},
+       keywords = {hydrodynamics, methods: numerical, software: simulations, galaxies: clusters, galaxies: fundamental parameters, galaxies: groups - tions, Astrophysics - Cosmology and Nongalactic Astrophysics, Astrophysics - Astrophysics of Galaxies},
+           year = 2023,
+          month = apr,
+         volume = {520},
+         number = {2},
+          pages = {3164-3186},
+            doi = {10.1093/mnras/stad342},
+  archivePrefix = {arXiv},
+         eprint = {2210.09978},
+   primaryClass = {astro-ph.CO},
+         adsurl = {https://ui.adsabs.harvard.edu/abs/2023MNRAS.520.3164A},
+        adsnote = {Provided by the SAO/NASA Astrophysics Data System}
+  }
+  """,
+]
